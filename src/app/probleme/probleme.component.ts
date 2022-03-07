66623116e1aab2e33d6940eb51b830d6ed7c38f1
nicolas barrette <nicolas.barrette@outlook.com>
2022-03-07 16:53:55 -0500

    t6 complet

---
+++ src/app/probleme/probleme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-probleme',
+  templateUrl: './probleme.component.html',
+  styleUrls: ['./probleme.component.css']
+})
+export class ProblemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
